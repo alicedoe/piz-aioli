53483d496da47952fd3021e56c341f663381ea40
aliceedn <alice.edn@gmail.com>
2017-02-12 11:13:23 +0100

    Commit netbeans

--- module/Pizza/src/Pizza/Entity/CpVilles.php
+++ module/Pizza/src/Pizza/Entity/CpVilles.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * CpVilles
  *
- * @ORM\Table(name="cp_villes", uniqueConstraints={@ORM\UniqueConstraint(name="ville_slug", columns={"ville_slug"})}, indexes={@ORM\Index(name="ville_departement", columns={"ville_departement"}), @ORM\Index(name="ville_nom", columns={"ville_nom"}), @ORM\Index(name="ville_nom_reel", columns={"ville_nom_reel"}), @ORM\Index(name="ville_code_postal", columns={"ville_code_postal"}), @ORM\Index(name="ville_nom_simple", columns={"ville_nom_simple"})})
+ * @ORM\Table(name="cp_villes", indexes={@ORM\Index(name="ville_departement", columns={"ville_departement"}), @ORM\Index(name="ville_nom", columns={"ville_nom"}), @ORM\Index(name="ville_nom_reel", columns={"ville_nom_reel"}), @ORM\Index(name="ville_code_postal", columns={"ville_code_postal"}), @ORM\Index(name="ville_nom_simple", columns={"ville_nom_simple"})})
  * @ORM\Entity
  */
 class CpVilles
@@ -27,13 +27,6 @@ class CpVilles
      * @ORM\Column(name="ville_departement", type="string", length=3, nullable=true)
      */
     private $villeDepartement;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="ville_slug", type="string", length=255, nullable=true)
-     */
-    private $villeSlug;
 
     /**
      * @var string
